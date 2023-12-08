e4d68f0cd7c6b4e6545a01316067035916851fdb
yahya73 <yahya.fhima@esprit.tn>
2023-12-08 18:03:44 +0100

    fix

--- controllers/postController.js
+++ controllers/postController.js
@@ -40,7 +40,7 @@ module.exports = {
     
       getAllPosts: async (req, res) => {
         try {
-          const posts = await Post.find();
+          const posts = await Post.find({type: 0});
           res.status(200).json(posts);
         } catch (error) {
           res.status(500).json(error);
