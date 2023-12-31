d3327d93c42b7e1095bbef064e04188f84c963d1
yahya73 <yahya.fhima@esprit.tn>
2023-12-31 18:23:16 +0100

    route

--- routes/post.js
+++ routes/post.js
@@ -6,7 +6,7 @@ const postController = require('../controllers/postController');
 // Create a new post
 
 // Update a post by ID
-router.put('/update', postController.updatePost);
+router.put('/:postId', postController.updatePost);
 
 // Delete a post by ID
 router.delete('/:postId', postController.deletePost);
